1b2778c338bd86880d1199af634fd81c2f797673
joofrederik <121954413+joofrederik@users.noreply.github.com>
2024-10-28 22:36:05 +0700

    video 10 mvc

---
+++ Video10/app/controllers/Mahasiswa.php
@@ -0,0 +1,37 @@
+<?php
+
+class Mahasiswa extends Controller
+{
+
+    public function index()
+    {
+        $data["judul"] = "Data mahasiswa";
+        $data['mhs'] = $this->model('Mahasiswa_model')->getAllMahasiswa();
+        $this->view('templates/header', $data);
+        $this->view('mahasiswa/index', $data);
+        $this->view('templates/footer');
+    }
+
+    public function detail($id)
+    {
+        $data["judul"] = "Detail mahasiswa";
+        $data['mhs'] = $this->model('Mahasiswa_model')->getMahasiswaById($id);
+        $this->view('templates/header', $data);
+        $this->view('mahasiswa/detail', $data);
+        $this->view('templates/footer');
+    }
+
+    public function tambah()
+    {
+
+        if ($this->model('Mahasiswa_model')->tambahDataMahasiswa($_POST) > 0) {
+            Flasher::setFlash('Berhasil', 'ditambahkan', 'success');
+            header('Location: ' . BASEURL . '/mahasiswa');
+            exit;
+        } else {
+            Flasher::setFlash('Gagal', 'ditambahkan', 'danger');
+            header('Location: ' . BASEURL . '/mahasiswa');
+            exit;
+        }
+    }
+}
