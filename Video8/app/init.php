152c19b5a4c2696f6c369a892e247c967f4a2d4b
joofrederik <121954413+joofrederik@users.noreply.github.com>
2024-10-28 22:29:40 +0700

    video 8 mvc

---
+++ Video8/app/init.php
@@ -0,0 +1,12 @@
+<?php
+
+// inisasi
+
+// Core adalah classnya
+require_once 'core/App.php';
+// controler ini utama, controllers extend
+require_once 'core/Controller.php';
+// database
+require_once 'core/Database.php';
+// KOnstanta
+require_once 'config/config.php';
